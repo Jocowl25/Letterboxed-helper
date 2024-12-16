c335b0ba5595f98bac5ca4c3a927c3bb590adcc4
Jocowl <jocowl25@gmail.com>
2024-12-16 21:05:31 +0000

    dot help maybe idk

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,13 +1,19 @@
-export const handler = async () => {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
-  
-    const response = await fetch(POKE_API)
-    const data = await response.json()
-  
+exports.handler = async () => {
+    const cheerio = require("cheerio")
+    const axios = require("axios")
+	const url = `https://www.nytimes.com/puzzles/letter-boxed`;
+	const { data } = await axios.get(url);
+	const $ = cheerio.load(data);
+	let nyt=$('body').text();
+    startstring=nyt.indexOf("{")
+    startparens = startstring + nyt.substring(startstring).indexOf("{")
+    endstring = startparens + nyt.substring(startparens).indexOf("}")
+    metadata = JSON.parse(nyt.substring(startparens,endstring)+"}")
+    string=nyt.substring(startparens,endstring)+"}"
     return {
       statusCode: 200,
       body: JSON.stringify({
-        data
-      })
+        message: string,
+      }),
     }
-  }
+}
