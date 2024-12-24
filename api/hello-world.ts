f6b7b7aa74265fcd483834279228c8fb96422389
Jocowl <jocowl25@gmail.com>
2024-12-24 16:20:32 +0000

    test to see if this works

--- api/hello-world.ts
+++ api/hello-world.ts
@@ -1,16 +1,3 @@
 export async function GET(request: Request) {
-  console.log("async function loaded")
-    const cheerio = require("cheerio")
-    const axios = require("axios")
-	const url = `https://www.nytimes.com/puzzles/letter-boxed`;
-	const { data } = await axios.get(url);
-	const $ = cheerio.load(data);
-	let nyt=$('body').text();
-    let startstring=nyt.indexOf("{")
-    let startparens = startstring + nyt.substring(startstring).indexOf("{")
-    let endstring = startparens + nyt.substring(startparens).indexOf("}")
-    let metadata = JSON.parse(nyt.substring(startparens,endstring)+"}")
-    let string=nyt.substring(startparens,endstring)+"}"
-    console.log(string)
-    return new Response(string)
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
     }
