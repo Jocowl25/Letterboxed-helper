b655e8445df118e1e901a07fe57393f632524287
Jocowl <jocowl25@gmail.com>
2024-12-24 16:04:10 +0000

    attempted vercel migration

---
+++ api/hello-world.ts
@@ -0,0 +1,19 @@
+export async function GET(request: Request) {
+    const cheerio = require("cheerio")
+    const axios = require("axios")
+	const url = `https://www.nytimes.com/puzzles/letter-boxed`;
+	const { data } = await axios.get(url);
+	const $ = cheerio.load(data);
+	let nyt=$('body').text();
+    let startstring=nyt.indexOf("{")
+    let startparens = startstring + nyt.substring(startstring).indexOf("{")
+    let endstring = startparens + nyt.substring(startparens).indexOf("}")
+    let metadata = JSON.parse(nyt.substring(startparens,endstring)+"}")
+    let string=nyt.substring(startparens,endstring)+"}"
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: string,
+      }),
+    }
+}
